9fa76cab1b0615b84005921f80de69376a50e06a
pubdreamcc <493310907@qq.com>
2019-08-07 18:18:57 +0800

    添加商品页

--- react-admin-client/src/views/product/add-update.jsx
+++ react-admin-client/src/views/product/add-update.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-
+import {Card, Icon, Button} from 'antd'
 export default class ProductAddUpdate extends Component {
   render() {
+    const isUpdate = true
+    const title = (<span><Button type='primary' onClick={() => this.props.history.goBack()} style={{marginRight: 10}}><Icon type='arrow-left'/></Button>{isUpdate ? '修改商品' : '添加商品'}</span>)
     return (
-      <div>
-        ProductAddUpdate
-      </div>
+      <Card title={title}>
+      </Card>
     )
   }
 }
