fa8f0859129b12e6305d3459b32198de0df2c5a8
pubdreamcc <493310907@qq.com>
2019-09-04 16:53:38 +0800

    echarts for react -- data visualization

--- react-admin-client/src/views/charts/Bar.jsx
+++ react-admin-client/src/views/charts/Bar.jsx
@@ -1,11 +1,71 @@
 import React, { Component } from 'react'
-
+import {
+  Card, Button
+} from 'antd'
+import ReactEcharts from 'echarts-for-react'
 export default class Bar extends Component {
+
+  state = {
+    sales: [60, 20, 70, 30, 45, 60, 54], // 每月的销售量
+    stores: [20, 34, 18, 50, 49, 20, 33] // 每月的库存
+  }
+
+  /*
+    更新库存和销量，（库存和销量在原先基础上 加 1）
+  */
+
+  update = () => {
+    this.setState((state) => ({
+      sales: state.sales.map(item => item + 1),
+      stores: state.stores.reduce((prev, curr) => {
+        prev.push(curr + 1)
+        return prev
+      }, [])
+    }))
+  }
+
+  /*
+    指定图表的配置项和数据
+  */
+  getOption = (sales, stores) => {
+    return {
+      title: { text: 'ECharts 入门实例' },
+      tooltip: {},
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      legend: {
+        data: ['库存', '销量']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: sales
+        },
+        {
+          name: '库存',
+          type: 'bar',
+          data: stores
+        }
+    ]
+    }
+  }
+
   render() {
+    const {stores, sales} = this.state
     return (
-      <div>
-        Bar
-      </div>
+      <Card
+        title={<Button type='primary' onClick={this.update}>更新</Button>}
+      >
+        <ReactEcharts
+          option={this.getOption(sales, stores)}
+        />
+      </Card>
     )
   }
 }
--- react-admin-client/src/views/charts/Line.jsx
+++ react-admin-client/src/views/charts/Line.jsx
@@ -1,11 +1,74 @@
 import React, { Component } from 'react'
-
+import ReactEcharts from 'echarts-for-react'
+import {Button, Card} from 'antd'
 export default class Line extends Component {
+
+  state = {
+    sales: [60, 20, 70, 30, 45, 60, 54], // 每月的销售量
+    stores: [20, 34, 18, 50, 49, 20, 33] // 每月的库存
+  }
+
+  /*
+    更新库存和销量，（库存和销量在原先基础上 加 1）
+  */
+
+  update = () => {
+    this.setState((state) => ({
+      sales: state.sales.map(item => item + 1),
+      stores: state.stores.reduce((prev, curr) => {
+        prev.push(curr + 1)
+        return prev
+      }, [])
+    }))
+  }
+
+
+  /*
+    指定图表的配置项和数据
+  */
+
+  getOption = (sales, stores) => ({
+    title: {
+      text: 'ECharts 入门实例'
+    },
+    tooltip: {},
+    legend: {
+      data: ['库存', '销量']
+    },
+    xAxis: {
+      type: 'category',
+      data: ['Mon', {value: 'Tue', textStyle: {fontSize: 20, color: '#1DA57A'}}, 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        name: '库存',
+        type: 'line',
+        data: stores,
+        itemStyle: {
+          color: '#1DA57A'
+        }
+      },
+      {
+        name: '销量',
+        type: 'line',
+        data: sales
+      }
+    ] 
+  })
+
   render() {
+    const {sales, stores} = this.state
     return (
-      <div>
-        Line
-      </div>
+      <Card
+        title={<Button type='primary' onClick={this.update}>更新</Button>}
+      >
+        <ReactEcharts 
+          option={this.getOption(sales, stores)}
+        />
+      </Card>
     )
   }
 }
